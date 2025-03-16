75f890248b38fdb7753055c28f0174c1f9b458ff
uzair <uzairahamed6@gmail.com>
2025-03-16 23:14:02 +0530

    aurena bday

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import ImageSlider from './Components/ImageSlider/ImageSlider'
+const icon = 'icon.jpg';
+const love = <img src={icon} height='50' ></img>
 function App() {
 
 
   return (
     <div>
-      <h1 style={{color: "black",
+      <h1 style={{color: "white",
       backgroundColor: 'rgb(115,79,150)',
       padding: "1%" ,
       display:'flex',
       justifyContent: 'center',
       fontFamily : 'revert-layer'}}>Happy Birthday Aurena!</h1>
+      <h1 style={{color: "white",
+      backgroundColor: 'rgb(115,79,150)',
+      padding: "1%" ,
+      display:'flex',
+      justifyContent: 'center',
+      marginTop:'-40px',
+      fontFamily : 'revert-layer'}}>made with{love}by cattos</h1>
       <ImageSlider />
     </div>
   )
